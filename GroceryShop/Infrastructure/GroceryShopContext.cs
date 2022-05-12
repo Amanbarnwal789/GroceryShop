6c4632f4ec178a0609ae3f2e33b461576c669844
Amanbarnwal789 <41892595+Amanbarnwal789@users.noreply.github.com>
2022-05-12 23:20:54 +0530

    db

---
+++ GroceryShop/Infrastructure/GroceryShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroceryShop.Infrastructure
+{
+    public class GroceryShopContext : DbContext
+    {
+        public GroceryShopContext(DbContextOptions<GroceryShopContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
